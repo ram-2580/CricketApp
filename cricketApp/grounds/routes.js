56b0da09475c63e520f05cc47db5a996833e5889
Rajeev Kumar Yadav <rajeev011yadav@gmail.com>
2019-06-04 08:10:18 +0530

    Image upload in ground route

--- cricketApp/grounds/routes.js
+++ cricketApp/grounds/routes.js
@@ -15,34 +15,15 @@ route.get('/add', isLoggedIn, async (req, res) => {
 })
 route.post('/add', isLoggedIn, async (req, res) => {
 
-
-    let context = {
-        'user': req.user,
-        'profile': await db.Profile.findOne({ where: { userId: req.user.id } })
-    }
-    console.log(req.body)
-    var data = {
-        name: req.body.name,
-        landmark: req.body.landmark,
-        city: req.body.city,
-        district: req.body.district,
-        state: req.body.state,
-        country: req.body.state,
-        email: req.body.email,
-        contact: req.body.contactnumber,
-        latitude: req.body.lat,
-        logitude: req.body.lon
-    }
-
     upload(req, res, (err) => {
         if (!err) {
             console.log(req.file);
             let filePath = trimedFilePath(req);
             console.log(filePath)
+            let data = req.body
             data['img_url'] = filePath + req.file.filename;
             console.log(data);
             db.ground.create(data).then(ground => {
-                context['ground'] = ground
                 req.flash('message', 'Ground is Added.')
                 res.redirect('/');
             });
--- cricketApp/server.js
+++ cricketApp/server.js
@@ -4,26 +4,20 @@ var multer = require('multer');
 var R = require('ramda');
 var { isLoggedIn } = require('./users/middleware');
 var { getUsersData } = require('./utils/user');
-var app = express();
 var path = require('path');
 var { usersData } = require('./utils/user');
 var passport = require('passport');
 var session = require('express-session');
-var bodyParser = require('body-parser');
 var env = process.env.NODE_ENV || "development";
 var db = require('./database');
 
 var sessionConfig = require(path.join(__dirname, 'config', 'config.json'))[env].session;
 
-
+var app = express();
 
 app.use(flash())
 //For BodyParser
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(express.urlencoded({ extended: true }));
 
 app.use(session({
     secret: sessionConfig.secret,
@@ -64,6 +58,7 @@ app.get('/', isLoggedIn, async function (req, res) {
     const context = {
         user: req.user,
         profile: await db.Profile.findOne({ where: { userId: req.user.id } }),
+        grounds: await db.ground.findAll(),
         messages: req.flash('message'),
         followers: followers,
         followings: followings
@@ -78,6 +73,10 @@ require('./config/passport/passport.js')(passport, require('./database').User);
 //Routes
 app.use('/users', require('./users'));
 app.use('/profile', require('./profile'));
+// app.post('/ground/add', (req, res) => {
+//     console.log(req.body);
+//     res.redirect('/')
+// })
 app.use('/ground', require('./grounds/routes.js'))
 
 
